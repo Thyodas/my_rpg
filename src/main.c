1f1c22eda1667976f8cdbaab40098f2ba9f19357
Thyodas <guillaume.hein@epitech.eu>
2022-04-29 22:53:25 +0200

    ADD: comment

--- src/main.c
+++ src/main.c
@@ -37,6 +37,27 @@ static void error_handler(int argc, char **argv)
     // TODO check si y'a pas d'entré
 }
 
+//      ,-.                ,-.             ,-.  
+//      `-'                `-'             `-'  
+//      /|\                /|\             /|\  
+//       |                  |               |   
+//      / \                / \             / \  
+//   Guillaume            Jean            Mehdy 
+//       |  T'es cringe    |                |   
+//       |<----------------|                |   
+//       |                 |                |   
+//       |       tg        |                |   
+//       |---------------->|                |   
+//       |                 |                |   
+//       |                 | Oui papa sucre |   
+//       |                 | <--------------|   
+//   Guillaume            Jean            Mehdy 
+//      ,-.                ,-.             ,-.  
+//      `-'                `-'             `-'  
+//      /|\                /|\             /|\  
+//       |                  |               |   
+//      / \                / \             / \  
+
 int main(int argc, char **argv)
 {
     game_t game;
