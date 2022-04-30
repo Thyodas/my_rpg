cc8a40946b99ead57cea422ab1c101ce5098164e
Remi MERGEN <remi.mergen@epitech.eu>
2022-04-30 19:49:27 +0200

    adding enemy_t in object.h

--- include/object.h
+++ include/object.h
@@ -12,6 +12,8 @@
     #include "entity.h"
     #include "inventory.h"
 
+    #define SLIME_PATH "assets/spritesheets/slime.png"
+
     enum id_object_type {
         TELEPORTER_OBJ,
         BUTTON_OBJ,
@@ -24,6 +26,24 @@
     } teleporter_t;
 
     typedef struct {
+        sfSprite *sprite;
+        sfTexture *texture;
+        sfIntRect *rect;
+    } graphics_t;
+
+    typedef struct {
+        int damage;
+        int life_points;
+    } stats_t;
+
+    typedef struct {
+        sfVector2i pos1;
+        sfVector2i pos2;
+        entity_t entity;
+        stats_t *stats;
+    } enemy_t;
+
+    typedef struct {
         entity_t entity;
         int speed;
         int health;
@@ -33,7 +53,7 @@
     typedef struct object_s {
         enum id_object_type id;
         void *data;
-        void (*handler)(); // TODO @Guillaume
+        void (*handler)(); //TODO @Guillaume
         void (*draw)();
     } object_t;
 
