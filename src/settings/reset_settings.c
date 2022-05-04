c47e7d6ca92867ac73b86856e0af9d5c2bdde3e3
iMeaNz <mehdy.morvan@epitech.eu>
2022-05-04 17:14:13 +0200

    ADD: Added settings function (not implemented)

---
+++ src/settings/reset_settings.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-MUL-200-STG-2-1-myrpg-guillaume.hein
+** File description:
+** reset_settings.c
+*/
+
+#include "rpg.h"
+
+void reset_settings(game_t *game)
+{
+    return;
+}
---
+++ src/settings/set_music_volume.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-MUL-200-STG-2-1-myrpg-guillaume.hein
+** File description:
+** set_music_volume.c
+*/
+
+#include "rpg.h"
+
+void set_music_volume(game_t *game)
+{
+    return;
+}
