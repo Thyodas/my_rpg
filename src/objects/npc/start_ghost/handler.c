71400feae2bb0ff0249197f6886e8ac355508c97
iMeaNz <mehdy.morvan@epitech.eu>
2022-05-07 01:41:08 +0200

    FEAT: Animated start ghost

--- src/objects/npc/start_ghost/handler.c
+++ src/objects/npc/start_ghost/handler.c
@@ -14,6 +14,40 @@ void dialogue_box_handler(game_t *game, dialogue_box_t *dialogue);
 float calc_distance(sfVector2f point1, sfVector2f point2);
 void dialogue_box_set_text(dialogue_box_t *dialogue, char *text);
 
+static void animate_ghost(game_t *game, npc_t *ghost)
+{
+    static long last_animation = 0;
+    long current_us = sfClock_getElapsedTime(game->clock->clock).microseconds;
+    double diff = (current_us - last_animation) / 1000000.0;
+    if (diff >= 0.5) {
+        ghost->entity.rect.left += ghost->entity.spritesheet_rect_x;
+        sfSprite_setTextureRect(ghost->entity.sprite, ghost->entity.rect);
+        if (ghost->entity.rect.left == (ghost->entity.spritesheet_width -
+                                        ghost->entity.spritesheet_rect_x))
+            ghost->entity.rect.left = 0;
+        last_animation = current_us;
+    }
+}
+
+static void breathing_ghost(game_t *game, npc_t *ghost)
+{
+    static long last_breath_in = 0;
+    static long last_breath_out = 0;
+    static int breath_state = 1;
+    long current_us = sfClock_getElapsedTime(game->clock->clock).microseconds;
+    sfVector2f scale = sfSprite_getScale(ghost->entity.sprite);
+    if ((current_us - last_breath_in) / 1000000.0 >= 0.3) {
+        scale.x += 0.03 * breath_state;
+        scale.y += 0.01 * breath_state;
+        sfSprite_setScale(ghost->entity.sprite, scale);
+        last_breath_in = current_us;
+    }
+    if ((current_us - last_breath_out) / 1000000.0 >= 1) {
+        breath_state = (breath_state == 1) ? -1 : 1;
+        last_breath_out = current_us;
+    }
+}
+
 float calc_distance_sprite(sfSprite *sprite1, sfSprite *sprite2)
 {
     return (calc_distance(sfSprite_getPosition(sprite1),
@@ -41,6 +75,8 @@ void npc_start_ghost_handler(game_t *game, object_t *self)
 {
     player_t *player = game->play->player->data;
     npc_t *npc = self->data;
+    breathing_ghost(game, npc);
+    animate_ghost(game, npc);
     if (calc_distance_sprite(player->entity.sprite, npc->entity.sprite) > 100) {
         npc->dialogue_index = 0;
         npc->is_talking = false;
