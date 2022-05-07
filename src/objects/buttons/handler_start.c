aed11290b7eaccbc50fdc3b4deb7459c25d5b38d
iMeaNz <mehdy.morvan@epitech.eu>
2022-05-07 05:21:53 +0200

    FEAT: Added state clicked on button

--- src/objects/buttons/handler_start.c
+++ src/objects/buttons/handler_start.c
@@ -15,12 +15,18 @@ void handle_button_event_start(game_t *game, object_t *button)
         sfSprite_getGlobalBounds(CAST_BUTTON(button->data)->sprite);
     sfBool contained = sfFloatRect_contains(&rect_text, mouse_pos.x, mouse_pos.y);
 
+    if (contained == sfTrue && sfMouse_isButtonPressed(sfMouseLeft))
+        sfSprite_setColor(CAST_BUTTON(button->data)->sprite,
+                        (sfColor){200, 200, 200, 255});
     if (contained == sfTrue && game->cursor->mouse_pressed == 1) {
         game->cursor->mouse_pressed = 0;
         CAST_BUTTON(button->data)->state = CLICKED;
+        sfSprite_setColor(CAST_BUTTON(button->data)->sprite, sfWhite);
         CAST_BUTTON(button->data)->on_click(game);
-    } else if (contained == sfTrue)
+    } else if (contained == sfTrue) {
         CAST_BUTTON(button->data)->state = HOVERED;
-    else
+    } else {
+        sfSprite_setColor(CAST_BUTTON(button->data)->sprite, sfWhite);
         CAST_BUTTON(button->data)->state = IDLE;
+    }
 }
