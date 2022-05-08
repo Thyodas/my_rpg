65675ef0fb6068a9fc89d72ac69b2913aec014f3
Thyodas <guillaume.hein@epitech.eu>
2022-05-08 21:25:47 +0200

    MODIF: change health of mobs

--- src/objects/enemy/props_enemy/stats_enemy.c
+++ src/objects/enemy/props_enemy/stats_enemy.c
@@ -12,7 +12,7 @@ stats_t blob_stats(void)
     stats_t stats;
 
     stats.damage = 1;
-    stats.life_points = 5;
+    stats.life_points = 18;
     stats.range_aggro = 10;
     stats.speed = 1;
     return stats;
@@ -23,7 +23,7 @@ stats_t slime_stats(void)
     stats_t stats;
 
     stats.damage = 1;
-    stats.life_points = 2;
+    stats.life_points = 6;
     stats.range_aggro = 50;
     stats.speed = 1;
     return stats;
@@ -45,7 +45,7 @@ stats_t skeleton_stats(void)
     stats_t stats;
 
     stats.damage = 3;
-    stats.life_points = 3;
+    stats.life_points = 9;
     stats.range_aggro = 50;
     stats.speed = 1;
     return stats;
